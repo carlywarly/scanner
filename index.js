da4557de373e76660917ed7258a5d750a5a48a74
Carl Armstrong <carl@email.com>
2021-06-13 20:34:36 +0100

    3

--- index.js
+++ index.js
@@ -92,7 +92,8 @@ setInterval(function() {
 
 setInterval(function() {
   if(token == "") {
-    var restURL = 'http://192.168.0.1/cgi-bin/luci/api/xqsystem/login?username=admin&password=' + myArgs[0];
+    var restURL = 'http://192.168.0.1/cgi-bin/luci/api/xqsystem/login?username=admin&password=' + myArgs[2];
+    console.log(restURL);
     request(restURL, { json: true }, (err, res, body) => {
       if (err) { return console.log(err); }
       token = body.token
